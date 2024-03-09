59a96d2524fc2607f68afa475dd3e359e3ab2ec9
Adam Driscoll <adam.r.driscoll@gmail.com>
2024-03-09 07:26:43 -0600

    Force

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 Install-Module -Name Microsoft.PowerShell.PSResourceGet -Force -SkipPublisherCheck -AllowClobber -Scope CurrentUser -ErrorAction SilentlyContinue
-Install-Module -Name PSHtml
+Install-Module -Name PSHtml -Force
 
 Remove-Item -Path "$PSScriptRoot/output/**/*.nupkg" -Recurse -Force -ErrorAction SilentlyContinue
 New-Item -Path "$PSScriptRoot/output" -ItemType Directory -Force
