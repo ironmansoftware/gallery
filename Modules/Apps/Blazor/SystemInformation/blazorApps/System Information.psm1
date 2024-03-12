7cf087c2519db791e076d93094e38a39ae29484a
Adam Driscoll <adam.r.driscoll@gmail.com>
2024-03-12 07:26:52 -0500

    Process Table Updates.

--- Modules/Apps/Blazor/SystemInformation/blazorApps/System Information.psm1
+++ Modules/Apps/Blazor/SystemInformation/blazorApps/System Information.psm1
@@ -10,8 +10,8 @@ $Variables["Processes"] = (Get-Process | ForEach-Object {
         [PSCustomObject]@{
             Id   = $_.Id
             Name = $_.Name 
-            VM   = $_.VM
-            NPM  = $_.NPM 
-            PM   = $_.PM
+            VM   = [Math]::Round($_.VM / 1KB, 2)
+            NPM  = [Math]::Round($_.NPM / 1KB, 2)
+            PM   = [Math]::Round($_.PM / 1KB, 2)
         }
     })
