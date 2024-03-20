fcda7b19dbc9e049cf6f52d85cbe13e4651931d7
Matthew Morrow <mmorrow@jackhenry.com>
2024-03-20 09:22:08 -0500

    Update required modules and publish resources

--- build.ps1
+++ build.ps1
@@ -1,11 +1,28 @@
 Install-Module -Name Microsoft.PowerShell.PSResourceGet -Force -SkipPublisherCheck -AllowClobber -Scope CurrentUser -ErrorAction SilentlyContinue
 
-Remove-Item -Path "$PSScriptRoot/output/**/*.nupkg" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$PSScriptRoot/output/*.nupkg" -Force #-ErrorAction SilentlyContinue
 New-Item -Path "$PSScriptRoot/output" -ItemType Directory -Force
 
 Unregister-PSResourceRepository -Name PSUScriptLibrary -ErrorAction SilentlyContinue
-Register-PSResourceRepository -Name PSUScriptLibrary -Uri "$PSScriptRoot/output" 
+Register-PSResourceRepository -Name PSUScriptLibrary -Uri "$PSScriptRoot/output"
 
+#get a list of dependencies
+# TODO: handle dependencies that we do not own, eg, install-module ImportExcel
+$dependencies = @('ActiveDirectory.Scripts')
+
+# Publish Dependencies First
+foreach($dependency in $dependencies) {
+    $dependencyPath = Get-ChildItem "$PSScriptRoot/**/$dependency*.psd1" -Recurse | Where-Object {$_.Name.Replace('.psd1', '') -eq $dependency}
+    if ($dependencyPath) {
+        Write-Host "Publishing $dependencyPath"
+        Publish-PSResource -Repository PSUScriptLibrary -Path $dependencyPath.FullName
+        $env:PSModulePath = $env:PSModulePath + ";$($dependencyPath.Directory)"
+    }
+}
+
+# Then publish the rest of the resources
 Get-ChildItem "$PSScriptRoot/**/*.psd1" -Recurse | ForEach-Object {
-    Publish-PSResource -Repository PSUScriptLibrary -Path $_.FullName
+    if ($_.BaseName -notin $dependencies) {
+        Publish-PSResource -Repository PSUScriptLibrary -Path $_.FullName
+    }
 }
