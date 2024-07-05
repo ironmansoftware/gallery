f59615a25b7bcbf56720e500ab055ced745fde95
Adam Driscoll <adam.r.driscoll@gmail.com>
2024-07-05 12:17:47 -0500

    Read display name properly.

--- build.ps1
+++ build.ps1
@@ -29,7 +29,7 @@ Function Build-RequiredModuleFiles {
                 Tags        = $Data.PrivateData.PSData.Tags
                 Icon        = $Data.PrivateData.PSData.IconUri
                 Functions   = $Data.FunctionsToExport
-                DisplayName = $Data.DisplayName
+                DisplayName = $Data.PrivateData.PSData.DisplayName
                 Widgets     = @()
             }
 
