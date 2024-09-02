7431d930518a8744a80d818797d2566a048a02d0
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2024-09-02 09:30:24 -0500

    Added Group Filter. (#72)

--- ActiveDirectory/ActiveDirectory.Security/.universal/roles.ps1
+++ ActiveDirectory/ActiveDirectory.Security/.universal/roles.ps1
@@ -1,3 +1,8 @@
-Get-ADGroup | ForEach-Object {
+$Filter = 'GroupCategory -eq "Security"'
+if ($PSUGroupFilter) {
+    $Filter = $PSUGroupFilter
+}
+
+Get-ADGroup -Filter $Filter | ForEach-Object {
     New-PSURole -Name $_.Name -ClaimType 'http://schemas.microsoft.com/ws/2008/06/identity/claims/groupsid' -ClaimValue $_.SID.Value
 }
